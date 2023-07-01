db6607c48e9d7ca899b6cb247f40d34629258745
坂本ポテコ <afa@afa.moe>
2023-07-01 23:31:57 +0800

    Allow IP version preference settings and added help & version (#5)
    * Allow IP version selection
    * Added help
    * Added version

--- main.cpp
+++ main.cpp
@@ -7,10 +7,44 @@
 #include <unistd.h>
 
 #include "core.h"
+#include "version/git.h"
 
-void print_version()
+void print_help_short(const char *me)
 {
-    std::fprintf(stderr, "VERSION PLACEHOLDER\n");
+    std::fprintf(stderr,
+        "Usage: %s -d wg_device -k peer_pubkey -h hostname -p port [-i interval] [-4] [-6]\n"
+        "       [-D] [-f] [-v] [--help]\n",
+        me);
+}
+
+void print_help_long_and_exit(const char *me)
+{
+    print_help_short(me);
+    std::fprintf(stderr,
+        "Periodically checks and updates WireGuard peer endpoint IP against hostname."
+        "\n"
+        "   -d, --device        the WireGuard device which has the peer whose endpoint is to be updated\n"
+        "   -k, --pubkey        the public key of the peer whose endpoint is to be updated\n"
+        "   -h, --hostname      the hostname of the peer endpoint, which will be periodically resolved\n"
+        "   -p, --port          the port of the endpoint\n"
+        "   -i, --interval      the interval between hostname resolution\n"
+        "   -4, --prefer-ipv4   prefer IPv4\n"
+        "   -6, --prefer-ipv6   prefer IPv6\n"
+        "   -D, --debug         Enable debug logging\n"
+        "   -f, --frontend      Run in frontend. Do not daemonize\n"
+        "   -v, --version       Print the version info\n"
+        "   --help              Print this help\n"
+        "\n"
+        "Report bugs on https://github.com/sakamoto-poteko/wg-peer-resolv-update/issues, or mail to Afa <afa@afa.moe>\n");
+    exit(EXIT_SUCCESS);
+}
+
+void print_version_and_exit()
+{
+    std::printf("wg-peer-resolv-update version %s.\n"
+                "Copyright (C) 2023, Afa Cheng <afa@afa.moe>\n",
+        git_Describe());
+    exit(EXIT_SUCCESS);
 }
 
 void parse_args(int argc, char **argv, ResolvUpdateConfig &config)
@@ -19,6 +53,8 @@ void parse_args(int argc, char **argv, ResolvUpdateConfig &config)
     bool pubkey_set = false;
     bool host_set = false;
     bool port_set = false;
+    bool is_prefer_v4_set = false;
+    bool is_prefer_v6_set = false;
     unsigned long interval = 0;
     unsigned long port = 0;
 
@@ -36,20 +72,20 @@ void parse_args(int argc, char **argv, ResolvUpdateConfig &config)
         { "prefer-ipv6", no_argument, nullptr, '6' },
         { "debug", no_argument, nullptr, 'D' },
         { "frontend", no_argument, nullptr, 'f' },
-        { "version", no_argument, nullptr, 'V' },
+        { "version", no_argument, nullptr, 'v' },
+        { "help", no_argument, nullptr, 0 },
         { 0, 0, 0, 0 }
     };
 
     while (1) {
         int option_index = 0;
-        c = getopt_long(argc, argv, "Vd:k:h:p:i:46Df", long_options, &option_index);
+        c = getopt_long(argc, argv, "vd:k:h:p:i:46Df", long_options, &option_index);
         if (c == -1)
             break;
 
         switch (c) {
-        case 'V':
-            print_version();
-            exit(EXIT_SUCCESS);
+        case 'v':
+            print_version_and_exit();
             break;
 
         case 'd':
@@ -92,15 +128,11 @@ void parse_args(int argc, char **argv, ResolvUpdateConfig &config)
             break;
 
         case '4':
-            config.prefer_ipv4 = true;
+            is_prefer_v4_set = true;
             break;
 
         case '6':
-            if (config.prefer_ipv4) {
-                fprintf(stderr, "Can't prefer both v4 and v6\n");
-                exit(EXIT_FAILURE);
-            }
-            config.prefer_ipv4 = false;
+            is_prefer_v6_set = true;
             break;
 
         case 'D':
@@ -111,11 +143,15 @@ void parse_args(int argc, char **argv, ResolvUpdateConfig &config)
             config.frontend = true;
             break;
 
+        case 0:
+            if (std::strcmp("help", long_options[option_index].name) == 0) {
+                print_help_long_and_exit(argv[0]);
+            }
         default:
             fprintf(stderr, "?? getopt returned character code 0%o ??\n", c);
         case '?':
             // unknown option
-            exit(EXIT_FAILURE);
+            goto print_help_and_exit_failure;
         }
     }
 
@@ -128,23 +164,42 @@ void parse_args(int argc, char **argv, ResolvUpdateConfig &config)
 
     if (!device_set) {
         fprintf(stderr, "wireguard device is required\n");
-        exit(EXIT_FAILURE);
+        goto print_help_and_exit_failure;
     }
 
     if (!pubkey_set) {
         fprintf(stderr, "wireguard peer public key is required\n");
-        exit(EXIT_FAILURE);
+        goto print_help_and_exit_failure;
     }
 
     if (!host_set) {
         fprintf(stderr, "peer hostname is required\n");
-        exit(EXIT_FAILURE);
+        goto print_help_and_exit_failure;
     }
 
     if (!port_set) {
         fprintf(stderr, "port is required\n");
+        goto print_help_and_exit_failure;
+    }
+
+    if (is_prefer_v4_set && is_prefer_v6_set) {
+        fprintf(stderr, "Can't prefer both v4 and v6\n");
         exit(EXIT_FAILURE);
     }
+
+    if (is_prefer_v4_set) {
+        config.ip_version_preference = IPVersionPreference::PreferV4;
+    } else if (is_prefer_v6_set) {
+        config.ip_version_preference = IPVersionPreference::PreferV6;
+    } else {
+        config.ip_version_preference = IPVersionPreference::NoPreference;
+    }
+
+    return;
+
+print_help_and_exit_failure:
+    print_help_short(argv[0]);
+    exit(EXIT_FAILURE);
 }
 
 int main(int argc, char **argv)
@@ -152,7 +207,7 @@ int main(int argc, char **argv)
     std::signal(SIGINT, sigint_handler);
 
     ResolvUpdateConfig config = {
-        .prefer_ipv4 = true,
+        .ip_version_preference = IPVersionPreference::NoPreference,
         .refresh_interval_ms = 1000,
     };
 
